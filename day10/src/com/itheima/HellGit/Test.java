763aa52a676a10e09fbe10eb09d52af3826592f9
Yougggggg <youyongmr@gmail.com>
2021-09-08 15:37:07 +0800

    a版本库使用htp协议提交

--- day10/src/com/itheima/HellGit/Test.java
+++ day10/src/com/itheima/HellGit/Test.java
@@ -8,5 +8,6 @@ public class Test {
         System.out.println("开始冲突");
         System.out.println("你不是冲突");
         System.out.println("尝试使用https协议推送");
+        System.out.println("a版本库开始尝试使用https协议推送");
     }
 }
