5c28d48fd616c65b5d018f4aaa12a7964c7c6d14
Yougggggg <youyongmr@gmail.com>
2021-09-08 15:56:35 +0800

    c版本库开始使用https

--- day10/src/com/itheima/HellGit/Test.java
+++ day10/src/com/itheima/HellGit/Test.java
@@ -8,5 +8,7 @@ public class Test {
         System.out.println("开始冲突");
         System.out.println("你不是冲突");
         System.out.println("尝试使用https协议推送");
+        System.out.println("c版本库尝试使用https协议推送");
+
     }
 }
