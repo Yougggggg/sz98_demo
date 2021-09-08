fcf894aff1623a53dc684bd39a9e34876a58831a
Yougggggg <youyongmr@gmail.com>
2021-09-08 13:41:39 +0800

    删除项目

--- day1_demo01/src/DemoA/DemoTest.java
+++
@@ -1,7 +0,0 @@
-package DemoA;
-
-public class DemoTest {
-    public static void main(String[] args) {
-
-    }
-}
--- day1_demo01/src/day22/MyMenuDemo.java
+++
@@ -1,7 +0,0 @@
-package day22;
-
-public class MyMenuDemo {
-    public static void main(String[] args) {
-
-    }
-}
